535e952a80188bfbaf0af250b84f769a5f16f68a
vipul <vipul.kumawat7@gmail.com>
2024-06-25 22:34:56 +0530

    initial commit

---
+++ src/test/java/com/thejavaengineer/checkstyle/CheckstyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejavaengineer.checkstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckstyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
